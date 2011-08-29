339f50af9dcfce2e1ad25aa9bbd68d48172ef525
johnnyquad <john@jhamill.com>
2011-08-29 23:18:53 +0100

    Arduino auto git save

--- USBJoystick.h
+++ USBJoystick.h
@@ -115,7 +115,24 @@ struct _joystick_data
 	uint8_t Unused2:4;
 	uint8_t Unused3:8;
 };
+
 typedef struct _joystick_data joystick_data;
+
+enum HatPositions
+{
+  HatCentre = 0xF;
+  HatNE = 0x1;
+  HatE = 0x2;
+  HatSE = 0x3;
+  HatS = 0x4;
+  HatSW = 0x5;
+  HatW = 0x6;
+  HatNW = 0x7;
+  HatN = 0x0;
+};
+
+
+
 /////////////////////////////////////////////////////////////////////
 /// \class USBJoystick USBJoystick.h <USBJoystick.h>
 /// \brief USB Interface to USB joystick/game pad, such as Logitech 'Dual Action' game pad
