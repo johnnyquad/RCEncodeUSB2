16cf0867959f25de6fc19e0057cd069c26c49bdd
johnnyquad <john@jhamill.com>
2011-09-01 15:26:59 +0100

    Arduino auto git save

--- RCEncoder.cpp
+++ RCEncoder.cpp
@@ -26,7 +26,7 @@ ISR(TIMER1_COMPA_vect) {
           digitalWrite(ledTest1,LOW);
           }
      	  digitalWrite(OutputPin, LOW); //change to HIGH for invert was LOW  
-	  OCR1A = Channels[Channel].ticks;
+	  OCR1A = Channels[Channel].ticks - MS_TO_TICKS(INTER_CHAN_DELAY;
           
           pulseState = stateHIGH;
    }
